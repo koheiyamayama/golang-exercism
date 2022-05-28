0be2503b703f1f56eef397f3af38ebcbb5ca98da
koheiyamayama <kohei.y.23@gmail.com>
2022-05-28 22:33:46 +0900

    complete isogram

---
+++ isogram/isogram.go
@@ -0,0 +1,19 @@
+package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+func IsIsogram(word string) bool {
+	word = strings.ToUpper(word)
+
+	for i, c := range word {
+		if unicode.IsLetter(c) && strings.ContainsRune(word[i+1:], c) {
+			return false
+		}
+	}
+
+	return true
+
+}
